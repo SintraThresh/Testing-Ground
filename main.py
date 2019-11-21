8ca8d7ee8e1f532ce1212c25fe95e9bd4c3e7ddf
TheWalkingPaco <brandonmtt125@hotmail.com>
2019-11-21 10:04:04 -0500

    Comment

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-sintra = 1
-gay = 1
-if sintra == gay:
-    print('This is working correctly')
-else:
-    print('Some shit is broken')
+UserNum = input('Enter number:')
+UserNum = int(UserNum)
+prob_fact = 1
+compositeLongName = 0
+Current_term = 0
+
+Highest_term = 0
+while Current_term < UserNum:
+    Current_term +=1
+    prob_fact = 1
+    #print('term: '+ str(Current_term))
+    while prob_fact <= Current_term:
+        if (Current_term % prob_fact) > 0:
+            prob_fact +=1
+        else:
+            compositeLongName +=1
+            prob_fact +=1
+    #print(str(Current_term) + ':' + str(compositeLongName))
+    #compositeLongName = 0
+    if compositeLongName > Highest_term:
+        Highest_term = compositeLongName
+        print(str(Current_term) + ':' + str(compositeLongName))
+    compositeLongName = 0
+
+
+    
