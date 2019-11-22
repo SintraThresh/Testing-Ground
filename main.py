78cb04eafd11d3e47782450956e4c9423ac65fd9
SintraThresh <vicentepsalcedo@gmail.com>
2019-11-22 10:36:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
-UserNum = input('Enter number:')
-UserNum = int(UserNum)
-prob_fact = 1
-compositeLongName = 0
-Current_term = 0
-
-Highest_term = 0
-while Current_term < UserNum:
-    Current_term +=1
+def antiprime_sequence():
+    UserNum = input('Enter number: ')
+    UserNum = int(UserNum)
     prob_fact = 1
-    while prob_fact <= Current_term:
-        if (Current_term % prob_fact) > 0:
-            prob_fact +=1
-        else:
-            compositeLongName +=1
-            prob_fact +=1
-    if compositeLongName > Highest_term:
-        Highest_term = compositeLongName
-        print(str(Current_term) + ':' + str(compositeLongName))
     compositeLongName = 0
+    Current_term = 0
 
+    Highest_term = 0
+    while Current_term < UserNum:
+        Current_term +=1
+        prob_fact = 1
+        while prob_fact <= Current_term:
+            if (Current_term % prob_fact) > 0:
+                prob_fact +=1
+            else:
+                compositeLongName +=1
+                prob_fact +=1
+        if compositeLongName > Highest_term:
+            Highest_term = compositeLongName
+            print(str(Current_term) + ':' + str(compositeLongName))
+        compositeLongName = 0
 
+Sequence_Selected = input("Which Math Sequence would you like?\n   1. Anti-Prime Sequence: ")
+Sequence_Selected = int(Sequence_Selected)
+
+if Sequence_Selected == 1:
+    antiprime_sequence()
+elif Sequence_Selected == 2
+else:
+    print("Please select a valid number option")
     
