cc2d2fae0730a65da8be586088273b2e7dc125a4
TheWalkingPaco <brandonmtt125@hotmail.com>
2019-11-26 17:10:54 -0500

    Create 6(Sum square difference).py

---
+++ 6(Sum square difference).py
@@ -0,0 +1,18 @@
+uInput = input('Enter number: ')
+
+n = 1
+numSum = 0
+while n <= int(uInput):
+    number = n
+    number *=number
+    numSum += number
+    n +=1
+n = 1
+number2 = 0
+while n <= int(uInput):
+    number2 += n
+    n += 1
+number2 *=number2
+solution = (number2 - numSum)
+print(str(solution))
+
