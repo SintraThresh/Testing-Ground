d697612e0eaea885223f30d8f9b4aea5e5b825fd
SintraThresh <46015685+SintraThresh@users.noreply.github.com>
2019-11-26 15:00:57 -0500

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,6 +1,16 @@
-
 n = 2
+halting_factor = 71
+
 while n <= 600851475143:
     if 600851475143 % n == 0:
+        test = 0
+        while test < n:
+            if n == 71:
+                pass
+            elif n % halting_factor == 0:
+                print("^That is the Highest prime factor of:^")
+                n = 600851475143
+                break
+            test += 1
         print(n)
     n += 1
