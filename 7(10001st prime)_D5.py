379fd80eed42206f77168d3d622bb404eeb5559e
SintraThresh <vicentepsalcedo@gmail.com>
2019-11-27 16:42:06 -0500

    gg no re

--- 7(10001st prime)_D5.py
+++ 7(10001st prime)_D5.py
@@ -1,12 +1,12 @@
+import math
 numInput = 10001
 cycle = 1
 start = 1
 while cycle <= numInput:
     start +=1
-    for i in range(2, start):          
-        if start != i: 
-            if (start % i) == 0:
-                break
+    for i in range(2, int(start**0.5 + 1)):
+        if (start % i) == 0:
+            break
     else:
         cycle+=1
 print(start)
