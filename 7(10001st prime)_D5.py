a408704ce4274fea9759fc5960f4f174d51af68a
TheWalkingPaco <brandonmtt125@hotmail.com>
2019-11-27 00:38:45 -0500

    7 finish

---
+++ 7(10001st prime)_D5.py
@@ -0,0 +1,13 @@
+uInput = input('Enter number: ')
+numInput = int(uInput)
+cycle = 1
+start = 1
+while cycle <= numInput:
+    start +=1
+    for i in range(2, start):          
+        if start != i: 
+            if (start % i) == 0:
+                break
+    else:
+        cycle+=1
+print(start)
