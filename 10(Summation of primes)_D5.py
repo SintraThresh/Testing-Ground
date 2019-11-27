741eabad19cbc08c7cb8441a17b9670aa3320391
SintraThresh <vicentepsalcedo@gmail.com>
2019-11-27 14:48:52 -0500

    Update 10(Summation of primes)_D5.py

--- 10(Summation of primes)_D5.py
+++ 10(Summation of primes)_D5.py
@@ -1,15 +1,17 @@
+import math
 numInput = 2000000
 cycle = 1
 start = 1
 number = 0
 while cycle < numInput:
     start +=1
-    for i in range(2, start//2):          
+    sqrt_start = math.sqrt(start)
+    sqrt_start = int(sqrt_start)
+    for i in range(2, sqrt_start):          
         if start != i: 
             if (start % i) == 0:
                 break
     else:
         number += start
-        print(start)
     cycle+=1
 print('big nibba' + str(number))
