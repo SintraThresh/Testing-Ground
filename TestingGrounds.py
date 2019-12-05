22fd73e5c5895ccfd5485f6d966d3e5c96eccc84
TheWalkingPaco <brandonmtt125@hotmail.com>
2019-12-05 02:15:06 -0500

    test stuff

--- TestingGrounds.py
+++ TestingGrounds.py
@@ -1,14 +1,31 @@
-grid = '08022297381500400075040507785212507791084949994017811857608717409843694804566200814931735579142993714067538830034913366552709523046011426924685601325671370236912231167151676389419236542240402866331380244732609903450244753353783684203517125032988128642367102638406759547066183864706726206802621220956394396308409166499421245558056673992697177878968314883489637221362309750076442045351400613397343133957817532822753167159403800462161409535692163905429635314755588824001754243629855786560048357189070544443744602158515417581980816805944769287392138652177704895540045208839735991607975732162626793327986688366887576220720346336746551232639353690442167338253911249472180846293240627636206936417230238834629969826759857404361620733529783190017431497148868116235705540170547183515469169233486143520189196748'
-def position(n):
-    min_pos = 0
-    max_pos = 1
-    n = int(n)
-    n *=2
-    min_pos +=int(n)
-    max_pos +=int(n)
-    aNumber = str(grid[min_pos])
-    bNumber = str(grid[max_pos])
-    cNumber = int(aNumber + bNumber)
-    return cNumber
-uInput = input('Enter number: ')
-print(position(uInput))
+from PyQt5.QtWidgets import QApplication, QPushButton, QLabel, QWidget, QVBoxLayout, QGridLayout, QMessageBox
+
+def maths():
+    apple = 1
+    banana = 2
+    both = banana + apple
+    return str(both)
+
+app = QApplication([])
+window =  QWidget()
+window.setGeometry(500,200,800,400)
+button1 =  QPushButton("Math")
+button2 = QPushButton("Clear")
+layout = QGridLayout()
+label = QLabel('The answer is ' + maths())
+def on_button_clicked():
+    layout.addWidget(label, 0, 1)
+    label.show()
+button1.clicked.connect(on_button_clicked)
+def clear_button():
+    label.hide()
+button2.clicked.connect(clear_button)
+
+
+layout.addWidget(button1, 0, 0)
+layout.addWidget(button2, 1, 0, 1, 2)
+button1.resize(100,32)
+
+window.setLayout(layout)
+window.show()
+app.exec_()
---
+++ TestingGrounds2.py
@@ -0,0 +1,2 @@
+array = [(a,b) for a in range(3) for b in range(3)]
+print(array)
