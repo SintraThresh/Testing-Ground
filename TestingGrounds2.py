099f1e6b19e426bf5d6586322538591e43d7f5a4
TheWalkingPaco <brandonmtt125@hotmail.com>
2019-12-12 10:14:47 -0500

    Update TestingGrounds2.py

--- TestingGrounds2.py
+++ TestingGrounds2.py
@@ -1,2 +1,8 @@
-array = [(a,b) for a in range(3) for b in range(3)]
-print(array)
+values = [1,2,3,4,5,6,7,8,9]
+testy = [(r,c) for r in range(3) for c in range(3)]
+
+n = 0
+for testy, value in zip(testy, values):
+    n += 1
+    print(str(n))
+print(testy)
